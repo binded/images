e96da1c5a903c77240f737a968af20fcba8ef1ee
Oli Lalonde <olalonde@gmail.com>
2017-06-19 01:54:17 -0700

    move function from env.sh to stdlib.sh

--- ci/base/1.0.0/rootfs/ci/lib/env.sh
+++ ci/base/1.0.0/rootfs/ci/lib/env.sh
@@ -1,38 +1,14 @@
 BUILD_DIR=${BUILD_DIR:-"."}
 
-git_repo_name() {
-  local url
-  local basename
-  local reponame
-  cd "$BUILD_DIR" || exit
-  url=$(git config --get remote.origin.url)
-  basename=$(basename "$url")
-  # my-repo.git
-  reponame=${basename%.*}
-  # my-repo
-  echo -n "$reponame"
-}
-
 validate_aws_env() {
   [ "$AWS_ACCESS_KEY_ID" ] || die "AWS_ACCESS_KEY_ID must be set"
   [ "$AWS_SECRET_ACCESS_KEY" ] || die "AWS_SECRET_ACCESS_KEY must be set"
 }
 
-guess_environment() {
-  cd "$BUILD_DIR"
-  branch=$(git symbolic-ref --short -q HEAD)
-
-  case "$branch" in
-  "master") echo -n "prod" ;;
-  "staging") echo -n "staging" ;;
-  *) echo -n "staging" ;;
-  esac
-}
-
 AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-"362178051443"}
 AWS_ECR_REGION=${AWS_ECR_REGION:-"us-west-1"}
 
-PROJECT_REPONAME=${PROJECT_REPONAME:-$(git_repo_name)}
+PROJECT_REPONAME=${PROJECT_REPONAME:-$(git_repo_name "$BUILD_DIR")}
 
 IMAGE_PREFIX=${IMAGE_PREFIX:-""}
 SHORT_VERSION=$(cd "${BUILD_DIR}" && git rev-parse --short HEAD)
@@ -47,17 +23,6 @@ IMAGE="${PRIVATE_REGISTRY}/${IMAGE_PREFIX}${SHORT_NAME}:${VERSION}"
 IMAGE_UNTAGGED="${PRIVATE_REGISTRY}/${IMAGE_PREFIX}${SHORT_NAME}"
 
 # For deployment
-ENVIRONMENT=${ENVIRONMENT:-$(guess_environment)}
+ENVIRONMENT=${ENVIRONMENT:-$(guess_environment "$BUILD_DIR")}
 KOPS_STATE_STORE=${KOPS_STATE_STORE:-"s3://binded-kops-${ENVIRONMENT}"}
 CLUSTER_NAME=${CLUSTER_NAME:-"${ENVIRONMENT}.b1nded.com"}
-
-export IMAGE
-export IMAGE_UNTAGGED
-export SHORT_NAME
-export SHORT_VERSION
-export VERSION
-export AWS_ECR_REGION
-export ENVIRONMENT
-export KOPS_STATE_STORE
-export CLUSTER_NAME
-export DEPLOY_PATH
--- ci/base/1.0.0/rootfs/ci/lib/stdlib.sh
+++ ci/base/1.0.0/rootfs/ci/lib/stdlib.sh
@@ -14,3 +14,31 @@ warn() {
 info() {
   echo "Info:" "$@"
 }
+
+git_branch() {
+  git symbolic-ref --short -q HEAD
+}
+
+guess_environment() {
+  cd "$1"
+  branch=$(git_branch)
+
+  case "$branch" in
+  "master") echo -n "prod" ;;
+  "staging") echo -n "staging" ;;
+  *) echo -n "staging" ;;
+  esac
+}
+
+git_repo_name() {
+  local url
+  local basename
+  local reponame
+  cd "$1" || exit
+  url=$(git config --get remote.origin.url)
+  basename=$(basename "$url")
+  # my-repo.git
+  reponame=${basename%.*}
+  # my-repo
+  echo -n "$reponame"
+}
