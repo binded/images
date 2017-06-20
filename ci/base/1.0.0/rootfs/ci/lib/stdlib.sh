3cc2bf9e98dda94a3e3354406b6a921c05c4d008
Oli Lalonde <olalonde@gmail.com>
2017-06-19 17:19:35 -0700

    docker_tag_exists function

--- ci/base/1.0.0/rootfs/ci/lib/stdlib.sh
+++ ci/base/1.0.0/rootfs/ci/lib/stdlib.sh
@@ -42,3 +42,34 @@ git_repo_name() {
   # my-repo
   echo -n "$reponame"
 }
+
+aws_ecr_curl() {
+  login_cmd=$(aws ecr get-login)
+  username=$(echo "$login_cmd" | cut -d " " -f 4)
+  password=$(echo "$login_cmd" | cut -d " " -f 6)
+  endpoint=$(echo "$login_cmd" | cut -d " " -f 9)
+
+  args=("$@")
+  args_length=${#args[@]}
+  args_last=${args[$args_length-1]}
+  unset 'args[${args_length}-1]'
+  path="${args_last}"
+
+  curl \
+    -u "${username}:${password}" \
+    "${args[@]}" \
+    "${endpoint}${path}"
+}
+
+# Usage: docker_tag_exists somerepo sometag
+docker_tag_exists() {
+  repo_name="$1"
+  tag="$2"
+  aws_ecr_curl \
+    --head \
+    --fail \
+    -s \
+    "/v2/${repo_name}/manifests/${tag}" \
+    > /dev/null
+}
+
